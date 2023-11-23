b6516056eed53c2fa476b3b01b3e2ad7a8165639
Loïse M <loise13@hotmail.fr>
2023-11-23 11:08:56 +0100

    ajout d'une table telephone

--- exo_contacts.sql
+++ exo_contacts.sql
@@ -1,19 +1,18 @@
-CREATE DATABASE IF NOT EXISTS 'exo_contacts' CHARACTER SET = 'utf8' COLLATE = 'utf8_general_ci';
+CREATE DATABASE IF NOT EXISTS exo_contacts CHARACTER SET = "utf8" COLLATE = "utf8_general_ci" ;
 
-SHOW DATABASE;
 
-USE exo_contacts;
+USE exo_contacts ;
 
 CREATE TABLE IF NOT EXISTS pays (
-iso_3 VARCHAR(3),
+iso_3 VARCHAR(3) PRIMARY KEY NOT NULL,
 nom VARCHAR(50),
 iso_2 VARCHAR(2),
-nationalite VARCHAR(50),
+nationalite VARCHAR(50)
 );
 
 INSERT INTO pays (iso_3, nom, iso_2, nationalite) VALUES
 ('DZA','Algerie','DZ','Algeriens'),
-('FRA','France','FR','Français'),
+('FRA','France','FR','Francais'),
 ('FIN','Finlande','FI','Finlandais'),
 ('DEU','Allemagne','DE','Allemands'),
 ('EGY','Egypte','EG','Egyptiens'),
@@ -26,19 +25,20 @@ INSERT INTO pays (iso_3, nom, iso_2, nationalite) VALUES
 SELECT * FROM pays;
 
 CREATE TABLE IF NOT EXISTS contacts (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    nom VARCHAR(20),
-    prenom VARCHAR(70),
-    date_de_naissance DATE,
+    id int PRIMARY KEY AUTO_INCREMENT,
+    nom VARCHAR(20) NOT NULL,
+    prenom VARCHAR(70) NOT NULL,
+    date_de_naissance DATE NOT NULL,
     sexe VARCHAR(10),
-    adresse TEXT,
-    code_postal VARCHAR(10),
-    ville VARCHAR(70),
-    pays_iso_3 VARCHAR(3),
-    FOREIGN KEY (pays_iso_3) REFERENCES pays(iso_3)
+    adresse TEXT NOT NULL,
+    code_postal VARCHAR(10) NOT NULL,
+    ville VARCHAR(70) NOT NULL,
+    pays_iso_3 VARCHAR(3) NOT NULL,
+    FOREIGN KEY(pays_iso_3) REFERENCES pays (iso_3)
 );
 
-INSERT INTO contacts (nom, prenom, date_de_naissance, sexe, adresse, code_postal, ville, pays_iso_3) VALUES
+INSERT INTO contacts (nom, prenom, date_de_naissance, sexe, adresse, code_postal, ville, pays_iso_3) 
+VALUES
 
 ("Garcia", "Pierre","23-05-1998","Homme","46 rue de la licorne", '13800','Istres','FRA'),
 ('Dupont','Marie', '15-04-1985', 'Femme', '123 Rue des Roses','10115', 'Berlin','DEU'),
@@ -51,3 +51,12 @@ INSERT INTO contacts (nom, prenom, date_de_naissance, sexe, adresse, code_postal
 ('Van Damme','Jean','20-11-1980','Homme','Avenue des Arts, 890','1040','Bruxelles','BEL'),
 ('Nasser','Fatima','14-07-1984','Femme','123 Rue du Nil','11511','Le Caire','EGY');
 
+SELECT * FROM contacts ;
+
+CREATE TABLE IF NOT EXISTS telephone (
+    id int PRIMARY KEY
+    id_contact int
+    numero VARCHAR(50),
+    type BYTE, 
+    
+)

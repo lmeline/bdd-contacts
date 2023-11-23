6602f15fc571dc3b7ab656d63a67ac4cc72898da
Loïse M <loise13@hotmail.fr>
2023-11-23 09:21:16 +0100

    ajout d'une table contacts

--- exo_contacts.sql
+++ exo_contacts.sql
@@ -3,3 +3,15 @@ CREATE DATABASE IF NOT EXISTS exo_contacts;
 SHOW DATABASE;
 
 USE exo_contacts;
+
+CREATE TABLE IF NOT EXISTS contacts (
+    id INT(11) USIGNED AUTO_INCREMENT PRIMARY KEY,
+    nom VARCHAR(20),
+    prenom VARCHAR(70),
+    date_de_naissance DATE,
+    sexe VARCHAR(10),
+    adresse TEXT,
+    code_postal VARCHAR(10),
+    ville VARCHAR(70),
+    pays_iso_3 VARCHAR(3),
+);

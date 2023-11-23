3045c629f7ad90debb5bb898b6f6877173abd86b
Loïse M <loise13@hotmail.fr>
2023-11-23 09:45:31 +0100

    ajout valeurs pays

--- exo_contacts.sql
+++ exo_contacts.sql
@@ -11,7 +11,19 @@ iso_2 VARCHAR(2),
 nationalite VARCHAR(50),
 );
 
-INSERT INTO pays (iso_3, nom) VALUES
+INSERT INTO pays (iso_3, nom, iso_2, nationalite) VALUES
+('DZA','Algerie','DZ','Algeriens'),
+('FRA','France','FR','Français'),
+('FIN','Finlande','FI','Finlandais'),
+('DEU','Allemagne','DE','Allemands'),
+('EGY','Egypte','EG','Egyptiens'),
+('HRV','Croatie','HR','Croates'),
+('ARG','Argentine','AR','Argentins'),
+('BOL','Bolivie','BO','Boliviens'),
+('BRA','Bresil','BR','Bresiliens'),
+('BEL','Belgique','BE','Belges');
+
+SELECT * FROM pays;
 
 CREATE TABLE IF NOT EXISTS contacts (
     id INT PRIMARY KEY AUTO_INCREMENT,

7fce9e7574f32922d0ba9c10fcaa8c7372a27afe
Loïse M <loise13@hotmail.fr>
2023-11-23 09:31:22 +0100

    ajout d'une table pays

--- exo_contacts.sql
+++ exo_contacts.sql
@@ -1,11 +1,20 @@
-CREATE DATABASE IF NOT EXISTS exo_contacts;
+CREATE DATABASE IF NOT EXISTS 'exo_contacts' CHARACTER SET = 'utf8' COLLATE = 'utf8_general_ci';
 
 SHOW DATABASE;
 
 USE exo_contacts;
 
+CREATE TABLE IF NOT EXISTS pays (
+iso_3 VARCHAR(3),
+nom VARCHAR(50),
+iso_2 VARCHAR(2),
+nationalite VARCHAR(50),
+);
+
+INSERT INTO pays (iso_3, nom) VALUES
+
 CREATE TABLE IF NOT EXISTS contacts (
-    id INT(11) USIGNED AUTO_INCREMENT PRIMARY KEY,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     nom VARCHAR(20),
     prenom VARCHAR(70),
     date_de_naissance DATE,
